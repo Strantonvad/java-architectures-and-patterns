be698dc926c3bfac65cf5418043bd5534602e017
Strantonvad <nottna@gmail.com>
2021-01-18 17:38:02 +0300

    homework6

---
+++ pattern/src/main/java/dataMapper/UserDataMapper.java
@@ -0,0 +1,13 @@
+package dataMapper;
+
+import java.util.Optional;
+
+public interface UserDataMapper {
+  Optional<User> find(int userId);
+
+  void insert(User user) throws DataMapperException;
+
+  void update(User user) throws DataMapperException;
+
+  void delete(User user) throws DataMapperException;
+}
